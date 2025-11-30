9045590dfa5bdd5e4a334586ea9597847a3bfe6b
Rovindu Thamuditha <rovinduthamu@gmail.com>
2025-11-30 18:14:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7270880107-59937",
+  "appId": "1:679818752988:web:e0826847ebd8af845f9519",
+  "apiKey": "AIzaSyDmu0dAjKMRympfmB2fe2HdY6Iom-A2Z84",
+  "authDomain": "studio-7270880107-59937.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "679818752988"
+};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,31 +1,50 @@
-import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+'use client';
 
-const firebaseConfig = {
-  "projectId": "studio-7270880107-59937",
-  "appId": "1:679818752988:web:e0826847ebd8af845f9519",
-  "apiKey": "AIzaSyDmu0dAjKMRympfmB2fe2HdY6Iom-A2Z84",
-  "authDomain": "studio-7270880107-59937.firebaseapp.com",
-  "storageBucket": "studio-7270880107-59937.appspot.com",
-  "messagingSenderId": "679818752988",
-};
+import { firebaseConfig } from '@/firebase/config';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
-let app: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
+export function initializeFirebase() {
+  if (!getApps().length) {
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
 
-function initializeFirebase() {
-  const apps = getApps();
-  if (!apps.length) {
-    app = initializeApp(firebaseConfig);
-  } else {
-    app = apps[0];
+    return getSdks(firebaseApp);
   }
-  auth = getAuth(app);
-  firestore = getFirestore(app);
 
-  return { app, auth, firestore };
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
-export { initializeFirebase };
+export function getSdks(firebaseApp: FirebaseApp) {
+  return {
+    firebaseApp,
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp)
+  };
+}
+
+export * from './provider';
+export * from './client-provider';
+export * from './firestore/use-collection';
+export * from './firestore/use-doc';
+export * from './non-blocking-updates';
+export * from './non-blocking-login';
+export * from './errors';
+export * from './error-emitter';
