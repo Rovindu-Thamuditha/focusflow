6e00865ea2bc45499c6d2e977cea8bfc36f41c06
Rovindu Thamuditha <rovindu2007@gmail.com>
2025-12-02 17:51:07 +0000

    Try fixing this error: `Runtime Error: Error: The default export is not

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/providers';
+import { Toaster } from "@/components/ui/toaster"
+import { FirebaseClientProvider } from '@/firebase/client-provider';
+
+import 'katex/dist/katex.min.css';
+
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+export const metadata: Metadata = {
+  title: 'GridFocus',
+  description: 'Track your focus and conquer your day.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          inter.variable
+        )}
+      >
+        <FirebaseClientProvider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+                <Toaster />
+            </ThemeProvider>
+        </FirebaseClientProvider>
+      </body>
+    </html>
+  );
+}
