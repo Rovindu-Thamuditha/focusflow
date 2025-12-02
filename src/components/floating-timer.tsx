020dfb5580987e0bd92e65f064e2253f4fc984e1
Rovindu Thamuditha <rovindu2007@gmail.com>
2025-12-02 17:24:07 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/floating-timer.tsx
+++ src/components/floating-timer.tsx
@@ -22,6 +22,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import type { Subject } from '@/lib/types';
