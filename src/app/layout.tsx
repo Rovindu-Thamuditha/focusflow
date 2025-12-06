0aa1e98b9fb267568a0c507a292b3984983a8f59
Rovindu Thamuditha <rovindu2007@gmail.com>
2025-12-06 05:21:57 +0000

    # 7) Use next/font/google for Inter
    Replace `<link>` in head with:
    
    ```

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-      </head>
+      <head/>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
