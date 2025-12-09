40769c3665ee51f7ba1d86b7523306c9b6d1723e
Rovindu Thamuditha <rovindu2007@gmail.com>
2025-12-09 07:56:04 +0000

    now I need to publish the website. make a stable optimized version , a p

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
