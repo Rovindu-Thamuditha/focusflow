644222aaed1ad0d6bf98ddfd1020b8277269e0b2
Rovindu Thamuditha <rovindu2007@gmail.com>
2025-12-09 07:51:02 +0000

    File changes

--- genkit-cli.ts
+++ genkit-cli.ts
@@ -1,3 +1,3 @@
-// Flows will be imported for their side effects in this file.
+// This file is used to run Genkit locally and is not part of the production build.
 import '@/ai/flows/send-feedback-flow';
 import '@/ai/flows/analyze-study-data-flow';
