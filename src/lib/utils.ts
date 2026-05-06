63673f4a02a3ec6fe14f17d7d05a04e1d963e688
Rovindu Thamuditha <rovindu2007@gmail.com>
2026-05-06 20:38:50 +0000

    ./src/components/main-header.tsx:96:94 Type error: Expected 1 arguments,

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const getInitials = (name: string | null | undefined) => {
-    if (name) {
-      return name.split(' ').map(n => n[0]).join('').substring(0, 2).toUpperCase();
+export const getInitials = (name: string | null | undefined, fallback?: string | null) => {
+    const text = name || fallback;
+    if (text) {
+      return text.split(' ').map(n => n[0]).join('').substring(0, 2).toUpperCase();
     }
     return '??';
 }
